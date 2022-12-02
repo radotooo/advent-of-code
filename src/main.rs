7e5236b5528f33f9a09a7b42105d140de8b39ebe
radotooo <radotooo@gmail.com>
2022-12-02 22:24:28 +0200

    day2

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
 use std::{fs::File, io::Read};
 
 fn main() {
-    let path = "calories.txt";
-    let data = find_most_calories(path);
+    let data = get_total_score();
 
     println!("{data}");
 }
 
-fn find_most_calories(path: &str) -> i32 {
+// day 1
+#[allow(dead_code)]
+fn find_most_calories() -> i32 {
+    let path = "calories.txt";
     let mut file = File::open(path).expect("Can't open file!");
     let mut contents = String::new();
     let mut result = Vec::new();
-    let mut temp = 0 ;
+    let mut temp = 0;
 
     file.read_to_string(&mut contents)
         .expect("Unable to read to line.");
@@ -28,5 +30,52 @@ fn find_most_calories(path: &str) -> i32 {
     result.sort_by(|a, b| b.cmp(a));
     // result[0] part1 answer
     result[0..3].iter().sum()
+}
 
+// day 2
+fn get_total_score() -> i32 {
+    let path = "rock_paper_scissors.txt";
+    let mut file = File::open(path).expect("Can't open file!");
+    let mut contents = String::new();
+    let mut part1 = 0;
+    let mut part2 = 0;
+
+    file.read_to_string(&mut contents)
+        .expect("Unable to read to line.");
+
+    for line in contents.lines() {
+         part1 += match line {
+            "A X" => 4,
+            "A Y" => 8,
+            "A Z" => 3,
+
+            "B X" => 1,
+            "B Y" => 5,
+            "B Z" => 9,
+
+            "C X" => 7,
+            "C Y" => 2,
+            "C Z" => 6,
+
+            _ => 0
+        };
+
+         part2 += match line {
+            "A X" => 3,
+            "A Y" => 4,
+            "A Z" => 8,
+
+            "B X" => 1,
+            "B Y" => 5,
+            "B Z" => 9,
+
+            "C X" => 2,
+            "C Y" => 6,
+            "C Z" => 7,
+
+            _ => 0
+        };
+    }
+
+    part2
 }
