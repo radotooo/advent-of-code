3f70e2eca0cd99339765bbed39c4612f2204fe8c
Radislav Danev <radotooo@gmail.com>
2023-12-06 00:45:28 +0200

    day5 part two

--- 2023/src/day5/day5.go
+++ 2023/src/day5/day5.go
@@ -3,12 +3,13 @@ package day5
 import (
 	"bufio"
 	"fmt"
+	"log"
+	"math"
+	"os"
 	"slices"
 	"sort"
 	"strconv"
 	"strings"
-	"log"
-	"os"
 )
 
 func Solve() {
@@ -21,7 +22,7 @@ func Solve() {
 
 	defer file.Close()
 
-	fmt.Println(partOne(sc))
+	fmt.Println(partTwo(sc))
 }
 
 func partOne(sc *bufio.Scanner) int {
@@ -51,7 +52,7 @@ func partOne(sc *bufio.Scanner) int {
 		}
 	}
 
-  keys := []int{}
+	keys := []int{}
 
 	for key := range ranges {
 		keys = append(keys, key)
@@ -63,12 +64,12 @@ func partOne(sc *bufio.Scanner) int {
 		for _, key := range keys {
 			for _, data := range ranges[key] {
 				destinationStart, rangeStart, length := data[0], data[1], data[2]
-        rangeEnd := rangeStart + length
+				rangeEnd := rangeStart + length
 
-        if seeds[i] >= rangeStart && seeds[i] <= rangeEnd { 
-          seeds[i] = (seeds[i] - rangeStart) + destinationStart
-          break
-        }
+				if seeds[i] >= rangeStart && seeds[i] <= rangeEnd {
+					seeds[i] = (seeds[i] - rangeStart) + destinationStart
+					break
+				}
 			}
 		}
 	}
@@ -76,8 +77,65 @@ func partOne(sc *bufio.Scanner) int {
 	return slices.Min(seeds)
 }
 
-func parseLine() {
+func partTwo(sc *bufio.Scanner) int {
+	ranges := map[int][][]int{}
+	rangeCount := 0
 
+	seeds := []int{}
+
+	for sc.Scan() {
+		line := sc.Text()
+
+		if strings.Contains(line, "seeds:") {
+			lineSplited := strings.Split(line, ":")
+			secondPart := lineSplited[1]
+
+			data, _ := parseData(strings.Fields(secondPart))
+			seeds = data
+
+		} else if line != "" {
+			data, err := parseData(strings.Fields(line))
+
+			if err != nil {
+				rangeCount++
+			} else {
+				ranges[rangeCount] = append(ranges[rangeCount], data)
+			}
+		}
+	}
+
+	keys := []int{}
+
+	for key := range ranges {
+		keys = append(keys, key)
+	}
+
+	sort.Ints(keys)
+	lowestValue := math.MaxUint32
+
+	for i := 0; i < len(seeds); i += 2 {
+		for j := seeds[i]; j < seeds[i]+seeds[i+1]; j++ {
+			value := j
+
+			for _, key := range keys {
+				for _, data := range ranges[key] {
+					destinationStart, rangeStart, length := data[0], data[1], data[2]
+					rangeEnd := rangeStart + length
+
+					if value >= rangeStart && value <= rangeEnd {
+						value = (value - rangeStart) + destinationStart
+						break
+					}
+				}
+			}
+
+			if value < lowestValue {
+				lowestValue = value
+			}
+		}
+	}
+
+	return lowestValue
 }
 
 func parseData(data []string) ([]int, error) {
